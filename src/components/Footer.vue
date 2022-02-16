a6e94f0d8a25752f4c8c72f50606ad0b728e7032
Vladis macbook <vladuxs1@gmail.com>
2022-02-16 16:35:08 +0200

    footer fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,28 @@
 <template>
   <footer class="mt-16">
     <div class="bg-black mb-[min(8%,100px)]">
-      <div class="container py-3 md:py-5">
-        <div class="flex items-center mb-2">
-          <a href="#" class="w-24 md:w-40 mr-5">
-            <img
-              class="h-auto w-full"
-              src="/img/instagram.svg"
-              alt="instagram"
-            />
+      <div class="container py-4 md:py-5">
+        <div class="flex items-center mb-2 justify-center md:justify-start">
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            class="mr-5 flex items-center"
+          >
+            <img class="w-7 h-7" src="/img/instagram.svg" alt="instagram" />
+            <span class="font-semibold text-xl m-3 text-white">Instagram</span>
           </a>
-          <a href="#" class="w-24 md:w-40 mr-5">
-            <img class="h-auto w-full" src="/img/facebook.svg" alt="facebook" />
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            class="flex items-center"
+          >
+            <img class="w-7 h-7" src="/img/fb.svg" alt="facebook" />
+            <span class="font-semibold text-xl m-3 text-white">Facebook</span>
           </a>
         </div>
-        <p class="text-white text-xs md:text-xl font-medium">
+        <p
+          class="text-white text-sm md:text-xl font-medium text-center md:text-left"
+        >
           Privacy policy. All rights reserver 2021.
         </p>
       </div>
